a7f0663017ab6cb88020e0e8ce194a52bb8960f3
garfield <cmgarfield@gmail.com>
2018-07-22 23:22:28 +0800

    Lec.370, Injecting the Script into the index.html file
    $ npm install --save-dev html-webpack-plugin

--- src/App.js
+++ src/App.js
@@ -13,14 +13,12 @@ class App extends Component {
     return (
       <div>
         <div>
-          <Link to="/">Users</Link> |
-          <Link to="/pizza">Pizza</Link>
+          <Link to="/">Users</Link> | <Link to="/pizza">Pizza</Link>
         </div>
         <div>
           <Route path="/" exact component={Users} />
           <Route path="/pizza" exact component={AsyncPizza} />
         </div>
-
       </div>
     );
   }
